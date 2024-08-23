783890f6662561a212b99d097e2520fd319295b2
alunogmfs2 <134213951+alunogmfs2@users.noreply.github.com>
2024-08-23 14:18:28 +0200

    u001.py

--- Pgzero/hallo.py
+++ Pgzero/hallo.py
@@ -6,8 +6,24 @@ alien.pos = 100, 56
 WIDTH = 500
 HEIGHT = alien.height + 20
 
+alien.topright = 0, 10
+
 def draw():
     screen.fill((255, 203, 219))
     alien.draw()
+
+def update():
+    alien.left = alien.left + 2
+    if alien.left > WIDTH:
+        alien.right = 0
+
+def on_mouse_down(pos):
+    if alien.collidepoint(pos):
+        sounds.eep.play()
+        alien.image = 'alien_hurt'
+        print('Ouch!')
+    else:
+        alien.image = 'alien'
+        print('Looser!')
 
 pgzrun.go()
